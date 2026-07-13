2bf0a961295e5006485c20e40cf81dd9bf8ea2dd
lharmeedey <kolawolebasit@gmail.com>
2026-07-13 16:56:23 +0100

    testing the real db

--- src/services/db.js
+++ src/services/db.js
@@ -1,9 +1,19 @@
 const { Pool } = require('pg');
+const { URL } = require('url');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
+
+
+
+
+const dbUrl = new URL(process.env.DATABASE_URL);
+
+console.log('DB Host:', dbUrl.hostname);
+console.log('DB Database:', dbUrl.pathname);
+
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
